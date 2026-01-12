9ce05c677b782f10e78dc09547f41f0437156c70
Eka Fajhari Adwar <eka.fajhari@qonex.id>
2026-01-12 10:02:02 +0700

    fixing 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
@@ -15,6 +16,11 @@ export default defineConfig({
         ws: true,
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
